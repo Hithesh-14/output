05e56185dfcdd737b9050ca44594b442bb54261e
Hithesh-14 <hitheshmuppala3216@gmail.com>
2025-12-19 07:07:48 +0530

    Implement method to add two integers and print result

---
+++ method with parameters and without return values.java
@@ -0,0 +1,17 @@
+class Demo {
+
+    int sum;
+
+    void add(int a, int b) {
+        sum = a + b;
+        System.out.println(sum);
+    }
+}
+
+public class ExampleMethod2 {
+    public static void main(String[] args) {
+
+        Demo d = new Demo();
+        d.add(10, 20);
+    }
+}
